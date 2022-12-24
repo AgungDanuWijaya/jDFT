10c411d55e007ffbcaee5029faaf253a30d70c32
Agung Danu Wijaya <38177351+AgungDanuWijaya@users.noreply.github.com>
2022-12-24 21:36:41 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'jDFT'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Agung Danu Wijaya'
 
 release = '0.1'
 version = '0.1.0'
